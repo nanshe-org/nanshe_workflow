1bbfb188fddb58e0630efcccdc22e9bea67e50cd
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-03-26 15:16:03 -0400

    Update teardown function
    Handle only the files that are now generated after switching to the
    Distributed Array Store.

--- teardown_tests.py
+++ teardown_tests.py
@@ -15,18 +15,6 @@ for each in list(sys.argv[1:]) + [
     "data_traces.h5",
     "data_rois.h5",
     "data.zarr",
-    "data_trim.zarr",
-    "data_dn.zarr",
-    "data_reg.zarr",
-    "data_sub.zarr",
-    "data_f_f0.zarr",
-    "data_wt.zarr",
-    "data_norm.zarr",
-    "data_dict.zarr",
-    "data_post.zarr",
-    "data_traces.zarr",
-    "data_rois.zarr",
-    "data_proj.zarr",
     "data_proj.html",
     "dask-worker-space"]:
     if os.path.isfile(each):
