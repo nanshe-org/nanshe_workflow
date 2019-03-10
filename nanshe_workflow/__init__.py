879c5465f0c48c18ec435f78d87569c01ea34e4f
John Kirkham <kirkhamj@janelia.hhmi.org>
2019-03-09 19:05:05 -0500

    Skip setting `DASK_CONFIG` environment variable
    Avoid hackily setting the `DASK_CONFIG` environment variable in the
    `nanshe_workflow.__init__` module as the custom `dask_config.yaml` file
    will no longer be included.

--- nanshe_workflow/__init__.py
+++ nanshe_workflow/__init__.py
@@ -4,9 +4,3 @@ __date__ = "$Oct 26, 2015 16:03$"
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
-
-import os
-os.environ["DASK_CONFIG"] = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "dask_config.yaml"
-)
-del os
